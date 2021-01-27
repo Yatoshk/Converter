270c0807303e026dfe58034c4fd11d9e71b9b38e
Yatoshk <chesalova2003@mail.ru>
2021-01-27 23:12:30 +0400

    corrected code idea

--- SMTH/SMTH.cpp
+++ SMTH/SMTH.cpp
@@ -1,73 +1,27 @@
 ﻿#include <iostream>
 using namespace std;
 
-int ToConvert(int NUMB, int ConvertSystem)
-{
-	int k = 1, resultNUMB = 0;
-	while (NUMB > 0)
-	{
-		resultNUMB += (NUMB % ConvertSystem) * k;
-		NUMB /= ConvertSystem;
-		k *= 10;
-	}
-	return resultNUMB;
-}
-
-int ToConvertCustom(int NUMB, int System, int ConvertSystem)
-{
-	int k = 0, i = 1, resultNUMB = 0;
-	int NUMB10 = 0;
-	while (NUMB > 0)
-	{
-		NUMB10 += (NUMB % 10) * (pow(System, k));
-		k++; 
-		NUMB /= 10;
-	}
-	k = 1;
-	while (NUMB10 > 0)
-	{
-		resultNUMB += (NUMB10 % ConvertSystem) * k;
-		NUMB10 /= ConvertSystem;
-		k *= 10;
-	}
-	return resultNUMB;
-}
+int Converter(int& const, int& , int& );
 
 int main()
 {
-	int number = 0, selectOption = 0, FirstSystem = 0, SecondSystem = 0;
-	cout << "1. System 10 into 2" << endl;
-	cout << "2. System 10 into 8" << endl;
-	cout << "3. System 10 into 16" << endl;
-	cout << "4. Your option" << endl;
-	cout << "Select option: "; cin >> selectOption;
-	switch (selectOption)
+	int numb, fsystem, ssystem;
+	cout << "Enter the number, its system and the system you want to convert to" << endl;
+	cout << "Number: _\b"; cin >> numb;
+	cout << "System First: _\b"; cin >> fsystem;
+	cout << "System Second: _\b"; cin >> ssystem;
+	cout << "Result: " << Converter(numb, fsystem, ssystem);
+	return 0;
+}
+
+int Converter(int &n, int &FromSystem, int &ToSystem)
+{
+	int x = 0, k = 0;
+	while (n > 0)
 	{
-	case 1:
-	{
-		cout << "Enter the number for convert: "; cin >> number;
-		cout << "Result: " << ToConvert(number, 2) << endl;
-		break;
+		x += (n % ToSystem) * pow(FromSystem, k);
+		k++;
+		n /= ToSystem;
 	}
-	case 2:
-	{
-		cout << "Enter the number for convert: "; cin >> number;
-		cout << "Result: " << ToConvert(number, 8) << endl;
-		break;
-	}
-	case 3:
-	{
-		cout << "Enter the number for convert: "; cin >> number;
-		cout << "Result: " << ToConvert(number, 16) << endl;
-		break;
-	}
-	case 4:
-	{
-		cout << "Enter the number to convert, its number system and the final system: "; cin >> number; cin >> FirstSystem; cin >> SecondSystem;
-		cout << "Result: " << ToConvertCustom(number, FirstSystem, SecondSystem) << endl;
-		break;
-	}
-	default:
-		break;
-	}
+	return x;
 }
